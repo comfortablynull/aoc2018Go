8130bc95a7624de0b970f219c87016c41cbe3d14
David Caiazzo <dc@comfortablynull.com>
2018-12-22 15:23:06 -0500

    pop

--- day9/main.go
+++ day9/main.go
@@ -1,13 +1,8 @@
-package day9
+package main
 
 import (
 	"fmt"
-	"math"
 )
-
-func Abs(x int) int {
-	return int(math.Abs(float64(x)))
-}
 
 type Node struct {
 	Value    int
@@ -15,37 +10,46 @@ type Node struct {
 	Previous *Node
 }
 
+func (n *Node) Insert(nn *Node) {
+	nn.Next = n.Next
+	n.Next.Previous = nn
+	n.Next = nn
+	nn.Previous = n
+}
+
+func (n *Node) Pop(b int) *Node {
+	if b > 0 {
+		b--
+		return n.Previous.Pop(b)
+	}
+	n.Previous.Next = n.Next
+	n.Next.Previous = n.Previous
+	return n
+}
+
 func main() {
-	mx := 71052*100
+	mx := 71052 * 100
 	max := (mx / 23) * 23
 	node := &Node{}
 	node.Next = node
 	node.Previous = node
 	numPlayers := 419
-	players := make([]int, numPlayers)
+	players := make(map[int]int)
 	highscore := 0
 	for i := 1; i <= max; i++ {
 		n := &Node{Value: i}
 		if i%23 == 0 {
-			for i := 0; i < 7; i++ {
-				node = node.Previous
-			}
+			node = node.Pop(7)
 			k := (i - 1) % numPlayers
 			players[k] += i
 			players[k] += node.Value
 			if players[k] > highscore {
 				highscore = players[k]
 			}
-			node.Previous.Next = node.Next
-			node.Next.Previous = node.Previous
 			node = node.Next
 			continue
 		}
-		in := node.Next
-		n.Next = in.Next
-		in.Next.Previous = n
-		in.Next = n
-		n.Previous = in
+		node.Next.Insert(n)
 		node = n
 	}
 	fmt.Println(highscore)
